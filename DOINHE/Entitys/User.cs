a16397fa1aa89318b78a5276a136c0ad13513c76
hùng nguyễn <manhhung02082003@gmail.com>
2024-09-12 00:30:36 +0700

    Add project files.

---
+++ DOINHE/Entitys/User.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace DOINHE.Entitys
+{
+    public class User
+    {
+        public int? Id { get; set; }
+
+        public string? Name { get; set; }
+
+        public string? Email { get; set; }
+
+        public string? Password { get; set; }
+
+        public string? UserName { get; set; }
+
+        public string? Phone { get; set; }
+
+        public int? Role { get; set; }
+
+        public double? Money { get; set;}
+        public virtual ICollection<Order> Orders { get; set; }
+    }
+}
