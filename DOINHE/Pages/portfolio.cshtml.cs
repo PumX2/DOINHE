50b545b756629bb65761eca3a95be4c245c600f4
hùng nguyễn <manhhung02082003@gmail.com>
2024-09-12 04:02:42 +0700

    Product - Detail

--- DOINHE/Pages/portfolio.cshtml.cs
+++ DOINHE/Pages/portfolio.cshtml.cs
@@ -1,12 +1,35 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using DOINHE.Entitys;
+using System.Diagnostics.Metrics;
+using System.Text.Json;
+using DOINHE.Db;
+using Azure;
 
 namespace DOINHE.Pages
 {
     public class portfolioModel : PageModel
     {
-        public void OnGet()
+        private readonly MyDbContext _db;
+
+        public portfolioModel(MyDbContext db)
         {
+            _db = db;
+        }
+
+        [BindProperty]
+        public List<Product> products { get; set; } = new List<Product>();
+
+        [BindProperty]
+        public List<Category> categories { get; set; } = new List<Category>();
+
+        public async Task<IActionResult> OnGetAsync()
+        {
+            int pageSize = 8;
+            products = _db.Products.ToList();
+            categories = _db.Categories.ToList();    
+            return Page();
         }
     }
 }
